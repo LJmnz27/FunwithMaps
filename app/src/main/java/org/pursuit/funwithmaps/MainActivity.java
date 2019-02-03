c9a6ca8ac5621c83d46ad3532b9328b69fae62fc
Luis Jimenez <LJmnz27@live.com>
2019-02-03 17:40:31 -0500

    initial commit

---
+++ app/src/main/java/org/pursuit/funwithmaps/MainActivity.java
@@ -0,0 +1,24 @@
+package org.pursuit.funwithmaps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.map_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
